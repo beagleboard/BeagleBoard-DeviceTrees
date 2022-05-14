41c5fd0f9ae1c42bae295b64561e8de907c07c06
Robert Nelson <robertcnelson@gmail.com>
2022-05-14 10:40:15 -0500

    ci: enable for gitlab
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -4,7 +4,7 @@ DIR="$PWD"
 
 dtc_git_build () {
 	project="dtc"
-	server="git://git.kernel.org/pub/scm/utils/dtc/dtc.git"
+	server="https://git.kernel.org/pub/scm/utils/dtc/dtc.git"
 
 	if [ ! -d ${HOME}/git/ ] ; then
 		mkdir -p ${HOME}/git/ || true
@@ -28,19 +28,18 @@ dtc_git_build () {
 		git checkout ${git_tag} -b ${git_tag}-build
 	fi
 
+	echo "make clean"
 	make clean
+	echo "make CC=gcc CROSS_COMPILE= all"
 	make CC=gcc CROSS_COMPILE= all &> /dev/null
 	cd ${DIR}/
 }
 
-git_tag="v1.6.0"
+git_tag="v1.6.1"
 dtc_git_build
 
 echo "*********************************************"
-echo "dtc: `/var/lib/jenkins/git/dtc/dtc --version`"
-DTC=/var/lib/jenkins/git/dtc/dtc DTCVERSION=2.0.0 make clean
-DTC=/var/lib/jenkins/git/dtc/dtc DTCVERSION=2.0.0 make all || exit 1
+echo "dtc: `~/git/dtc/dtc --version`"
+DTC=~/git/dtc/dtc DTCVERSION=2.0.0 make clean
+DTC=~/git/dtc/dtc DTCVERSION=2.0.0 make all || exit 1
 echo "*********************************************"
-
-unset git_tag
-dtc_git_build
