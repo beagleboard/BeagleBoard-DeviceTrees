ec0d6b88ebb0c4f1caeb5f4e4a982364a3c8b634
Robert Nelson <robertcnelson@gmail.com>
2022-07-30 17:15:55 -0500

    v5.10.x-ti-arm64: keep in sync
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ scripts/pull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git pull --rebase --no-edit git@github.com:beagleboard/BeagleBoard-DeviceTrees.git v5.10.x-ti-arm64
+git pull --rebase --no-edit git@git.beagleboard.org:beagleboard/BeagleBoard-DeviceTrees.git v5.10.x-ti-arm64
