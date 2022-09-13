e8339ead86d7e44978f2eb54097b60b5038d6e02
Robert Nelson <robertcnelson@gmail.com>
2022-09-13 13:56:11 -0500

    update pull script
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git pull --rebase --no-edit git@github.com:beagleboard/BeagleBoard-DeviceTrees.git v5.10.x-ti
-git pull --rebase --no-edit git@git.beagleboard.org:beagleboard/BeagleBoard-DeviceTrees.git v5.10.x-ti
+git pull --rebase --no-edit git@github.com:beagleboard/BeagleBoard-DeviceTrees.git v5.10.x-ti-unified
+git pull --rebase --no-edit git@git.beagleboard.org:beagleboard/BeagleBoard-DeviceTrees.git v5.10.x-ti-unified
