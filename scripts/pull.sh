79560609c9992bfa84149fe9032af57ce052e373
Robert Nelson <robertcnelson@gmail.com>
2026-09-15 11:30:31 -0500

    merge: v6.1.x
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-git pull --no-edit https://github.com/beagleboard/BeagleBoard-DeviceTrees.git v5.15.x
-git pull --no-edit https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees.git v5.15.x
+branch="v5.15.x"
+
+git pull --no-edit https://github.com/beagleboard/BeagleBoard-DeviceTrees.git ${branch}
+git pull --no-edit https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees.git ${branch}
 
